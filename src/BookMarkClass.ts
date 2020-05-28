cfa2df80197e23f9f2901662bf5bdb700b588b4f
Matthew-swin <103219809@swin.edu.au>
2020-05-28 12:18:40 +1000

    basics started/testing

---
+++ src/BookMarkClass.ts
@@ -0,0 +1,20 @@
+/*export class BookMark {
+
+title: string;
+icon: string;
+favourite: boolean = false;
+
+constructor (title: string, icon:string){
+    this.title = title
+    this.icon = icon
+}
+
+setUrl(url: string){
+
+}
+getUrl(){
+
+}
+
+}
+*/
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
-let myButton = document.getElementById("myButton") as HTMLButtonElement | null;
-if (myButton == null) {
+//import {BookMark} from "./BookMarkClass"
+let CreateBookMark = document.getElementById("CreateBookMark") as HTMLButtonElement | null;
+let container = document.getElementById("container") as HTMLDivElement | null;
+let NewBookMark = document.getElementById("NewBookMark") as HTMLDivElement | null;
+
+
+if (CreateBookMark == null) {
     alert("Button not found.");
 } else {
     
-    myButton.onclick = function () {
-        if(myButton == undefined) return;
-        alert("Hello World!");
+    CreateBookMark.onclick = function () {
+        if(CreateBookMark == undefined) return;
+        //new bookMark
+        let poop = document.createElement("p")
+        poop.classList.add('yafucked')
+        poop.innerText = 'I will cut you'
+
+        let
+        NewBookMark?.appendChild(poop)
+        
     }
+
+    /*myButton2.onclick = function (event) {
+        event.preventDefault();
+        if(myButton2 == undefined || input == null || container == null ) return;
+        const newOne = document.createElement('li')
+        newOne.classList.add('NewOneClass');
+        newOne.innerText = input.value;
+        container.appendChild(newOne);
+
+        
+    }*/
 }
