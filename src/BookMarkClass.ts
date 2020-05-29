3019e731783aff28f223e793ef70f1a5f02d71d4
Matthew-swin <103219809@swin.edu.au>
2020-05-29 19:03:02 +1000

    starting on delete

--- src/BookMarkClass.ts
+++ src/BookMarkClass.ts
@@ -46,23 +46,46 @@ let NewBookMark = document.getElementById("NewBookMark") as HTMLDivElement | nul
 let URL = this.bookmark.icon;
 //setting up icon image
 let img = document.createElement("img") as HTMLImageElement;
-img.setAttribute('src', URL)
+img.setAttribute('src', URL);
 this.element.appendChild(img);
-//the start of the appendaning with title and URL
-this.element.appendChild(document.createTextNode(this.bookmark.title))
-this.element.appendChild(document.createTextNode(" "))
-this.element.appendChild(document.createTextNode(id2))
+//setting up bookmark title
+let title = document.createElement("h4");
+title.appendChild(document.createTextNode(this.bookmark.title));
+this.element.appendChild(title);
+//setting up url
+let url = document.createElement("h6");
+url.appendChild(document.createTextNode(URL));
+this.element.appendChild(document.createTextNode(id2));
+// setting up favourites icon
+let favIcon=document.createElement('button');
+favIcon.innerHTML = '<i class="fas fa-star"></i>';
+favIcon.classList.add("favIcon")
+this.element.appendChild(favIcon);
+// setting up delete icon
+//Still working on this
+///////////////////////////////////////////////////
+let DeleteIcon=document.createElement('button');
+DeleteIcon.innerHTML = '<i class="fas fa-trash"></i>';
+DeleteIcon.classList.add("DeleteIcon")
+this.element.appendChild(DeleteIcon);
+////////////////////////////////////////////////////
 //bottom bitch
 NewBookMark?.appendChild(this.element);
 
 
 }
 
-deleteBookMark(){
+deleteBookMark(e: { target: any; }){
+    let item = e.target;
+    if (item == null) return;
+    if (item.classList[0] === 'DeleteIcon') {
+        let del = item.parentElement;
+        del.remove();
+    }
 
 }
 
-toggleFavourites(id : string){
+toggleFavourites(){
 
 }
 }
