89552c1c215a237f86043a0ba1f4e41b19ec5e94
Matthew-swin <103219809@swin.edu.au>
2020-05-29 16:29:48 +1000

    done with them icons son

--- src/BookMarkClass.ts
+++ src/BookMarkClass.ts
@@ -1,20 +1,59 @@
-/* export class BookMark {
+export class BookMark {
 
 title: string;
 icon: string;
 favourite: boolean = false;
 
-constructor (title: string, icon:string){
-    this.title = title
-    this.icon = icon
+constructor (ptitle: string, picon:string){
+    this.title = ptitle;
+    this.icon = "";
+    let URL = document.getElementById("URL") as HTMLInputElement | null;
+    var URL2 = URL?.value;
+    if (URL2 == null ){
+    console.log("damn son!")
+    
+} else{
+    let picon =  URL2.replace('http://','').replace('https://','').replace('www\.','').split(/[/?#]/);
+    let domain = picon[0];
+    this.icon = `https://s2.googleusercontent.com/s2/favicons?domain=${domain}`;
+}
+}
+setURL(id: string){
+let URL = document.getElementById("URL") as HTMLInputElement | null;
+
 }
 
-setUrl(url: string){
-
-}
-getUrl(){
+getURL(){
 
 }
 
 } 
- */
+
+export class BookMarkFun {
+
+bookmark: BookMark;
+element: HTMLElement | null = null;
+
+
+constructor(pBookmark: BookMark){
+this.bookmark = pBookmark;
+}
+
+Spawn(id: string, id2: string){
+this.element = document.createElement("div");
+let NewBookMark = document.getElementById("NewBookMark") as HTMLDivElement | null;
+let URL = this.bookmark.icon;
+let img = document.createElement("img") as HTMLImageElement;
+img.setAttribute('src', URL)
+this.element.appendChild(img);
+this.element.appendChild(document.createTextNode(this.bookmark.title))
+this.element.appendChild(document.createTextNode(id2))
+//bottom bitch
+NewBookMark?.appendChild(this.element);
+
+
+}
+toggleFavourites(id : string){
+
+}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,64 @@
-//import {BookMark} from "./BookMarkClass"
+import { BookMark, BookMarkFun } from "./BookMarkClass"
 let InputPopup = document.getElementById("InputPopup") as HTMLButtonElement | null;
 let ClosePopup = document.getElementById("ClosePopup") as HTMLButtonElement | null;
-let container = document.getElementById("container") as HTMLDivElement | null;
+let AddBookMark = document.getElementById("AddBookMark") as HTMLButtonElement | null;
 let NewBookMark = document.getElementById("NewBookMark") as HTMLDivElement | null;
 let PopupPanel = document.getElementById("PopupPanel") as HTMLDivElement | null;
+let Title = document.getElementById("Title") as HTMLInputElement | null;
+let URL = document.getElementById("URL") as HTMLInputElement | null;
+
+
+
 
 
 if (InputPopup == null || undefined) {
     alert("Button not found.");
 } else {
-    InputPopup.onclick = function (id){
-    if (PopupPanel == null || event == null) return;
-    event.preventDefault();
-    PopupPanel.style.display = "block";
-} 
+    InputPopup.onclick = function (id: MouseEvent) {
+        if (PopupPanel == null || event == null) return;
+        event.preventDefault();
+        PopupPanel.style.display = "block";
+    }
 
-if (ClosePopup == null || undefined) {
+    if (ClosePopup == null || undefined) {
+        alert("Button not found.");
+    } else {
+        ClosePopup.onclick = function (id: MouseEvent) {
+            if (PopupPanel == null || event == null) return;
+            event.preventDefault();
+            PopupPanel.style.display = "none";
+        }
+
+        if (AddBookMark == null || undefined) {
+            alert("Button not found.");
+        } else {
+            AddBookMark.onclick = function () {
+                if (AddBookMark == null || event == null || PopupPanel == null || Title == null || URL == null) return;
+                PopupPanel.style.display = "none";
+                let a = Title.value;
+                let b = URL.value;
+                let bookMark = new BookMark(a, b);
+                let bookMarkFun = new BookMarkFun(bookMark)
+                bookMarkFun.Spawn(a,b);
+                Title.value = "";
+                URL.value = "";
+                
+                
+                
+
+            }
+
+
+        }
+    }
+
+}
+
+/* if (AddBookMark == null) {
     alert("Button not found.");
 } else {
-    ClosePopup.onclick = function (id){
-    if (PopupPanel == null || event == null) return;
-    event.preventDefault();
-    PopupPanel.style.display = "none";
-} 
 
- 
-}
-}
-
-/* if (CreateBookMark == null) {
-    alert("Button not found.");
-} else {
-
-    CreateBookMark.onclick = function () {
+    AddBookMark.onclick = function () {
         if(CreateBookMark == undefined) return;
         //new bookMark
         let poop = document.createElement("p");
